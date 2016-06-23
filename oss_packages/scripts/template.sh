ad327416a16620208763637f073ce114483a779a
Michael Kress <m-kress@m-kress.de>
2016-06-23 15:13:17 +0200

    added template build script; added iptables build script

---
+++ oss_packages/scripts/template.sh
@@ -0,0 +1,50 @@
+#! /bin/sh
+
+# download link for the sources to be stored in dl directory
+PKG_DOWNLOAD=""
+
+# md5 checksum of archive in dl directory
+PKG_CHECKSUM=""
+
+# name of directory after extracting the archive in working directory
+PKG_DIR="project_name"
+
+# name of the archive in dl directory
+PKG_ARCHIVE_FILE="${PKG_DIR}.tar.gz"
+
+SCRIPTSDIR="$(dirname $0)"
+HELPERSDIR="${SCRIPTSDIR}/helpers"
+TOPDIR="$(realpath ${SCRIPTSDIR}/../..)"
+
+. ${TOPDIR}/scripts/common_settings.sh
+. ${HELPERSDIR}/functions.sh
+
+PKG_ARCHIVE="${DOWNLOADS_DIR}/${PKG_ARCHIVE_FILE}"
+PKG_SRC_DIR="${SOURCES_DIR}/${PKG_DIR}"
+PKG_BUILD_DIR="${BUILD_DIR}/${PKG_DIR}"
+PKG_INSTALL_DIR="${PKG_BUILD_DIR}/install"
+
+configure()
+{
+    cd "${PKG_BUILD_DIR}"
+    #export CROSS_COMPILE="${M3_CROSS_COMPILE}"
+    export CFLAGS="${M3_CFLAGS} -L${STAGING_LIB} -I${STAGING_INCLUDE}"
+    export LDFLAGS="${M3_LDFLAGS} -L${STAGING_LIB}"
+    ./configure --target=${M3_TARGET} --host=${M3_TARGET} --prefix=""
+}
+
+compile()
+{
+    copy_overlay
+    cd "${PKG_BUILD_DIR}"    
+    make ${M3_MAKEFLAGS} || exit_failure "failed to build ${PKG_DIR}"
+    make DESTDIR="${PKG_INSTALL_DIR}" install
+}
+
+install_staging()
+{
+    cd "${PKG_BUILD_DIR}"
+    make -i DESTDIR="${STAGING_DIR}" install || exit_failure "failed to install ${PKG_DIR}"
+}
+
+. ${HELPERSDIR}/call_functions.sh
