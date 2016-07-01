57403744675d95627a014f62a1e81f939f24f3d4
Michael Kress <m-kress@m-kress.de>
2016-07-01 15:35:36 +0200

    updated python to 2.7.12

--- oss_packages/scripts/python-2.7.12.sh
+++ oss_packages/scripts/python-2.7.12.sh
@@ -1,13 +1,13 @@
 #! /bin/sh
 
 # download link for the sources to be stored in dl directory
-PKG_DOWNLOAD="https://www.python.org/ftp/python/2.7.11/Python-2.7.11.tar.xz"
+PKG_DOWNLOAD="https://www.python.org/ftp/python/2.7.12/Python-2.7.12.tar.xz"
 
 # md5 checksum of archive in dl directory
-PKG_CHECKSUM="1dbcc848b4cd8399a8199d000f9f823c"
+PKG_CHECKSUM="57dffcee9cee8bb2ab5f82af1d8e9a69"
 
 # name of directory after extracting the archive in working directory
-PKG_DIR="Python-2.7.11"
+PKG_DIR="Python-2.7.12"
 
 # name of the archive in dl directory
 PKG_ARCHIVE_FILE="${PKG_DIR}.tar.xz"
@@ -26,31 +26,30 @@ PKG_INSTALL_DIR="${PKG_BUILD_DIR}/install"
 
 configure()
 {
+    copy_overlay
+
     cd "${PKG_BUILD_DIR}"
     export CFLAGS="${M3_CFLAGS} -I${STAGING_INCLUDE} -I${STAGING_DIR}/usr/include/ncurses -I${STAGING_DIR}/usr/include/ncursesw"
-    export LDFLAGS="${M3_LDFLAGS} -L${STAGING_LIB} "
+    export LDFLAGS="${M3_LDFLAGS} -L${STAGING_LIB}"
     export ac_cv_file__dev_ptc=no
     export ac_cv_file__dev_ptmx=no
     export CXX
 
-    ./configure --target=${M3_TARGET} --host=${M3_TARGET} --build=i686-pc-linux-gnu --with-fpectl --enable-shared --enable-ipv6 --with-threads --enable-unicode=ucs4 --with-computed-gotos --with-system-expat --without-ensurepip -- c_cv_file__dev_ptmx=no ac_cv_file__dev_ptc=no ac_cv_have_long_long_format=yes
+    ./configure --target=${M3_TARGET} --host=${M3_TARGET} --build=i686-pc-linux-gnu --with-fpectl --enable-ipv6 --with-threads --enable-unicode --with-computed-gotos --with-system-expat --with-lto
 }
 
 compile()
 {
-    copy_overlay
     cd "${PKG_BUILD_DIR}"
     touch Include/graminit.h Python/graminit.c
-    #make ${M3_MAKEFLAGS} || exit_failure "failed to build ${PKG_DIR}"
-    export PYTHONHOME="/bin"
-    export PYTHONPATH="/usr/lib/python2.7"
-
+    make ${M3_MAKEFLAGS} || exit_failure "failed to build ${PKG_DIR}"
     make DESTDIR="${PKG_INSTALL_DIR}" install
 }
 
 install_staging()
 {
-    cp -a "${PKG_INSTALL_DIR}/"* "${STAGING_DIR}"
+    rm -rf "${STAGING_DIR}/lib/libpython2"*
+    cp -r "${PKG_INSTALL_DIR}/usr/local/"* "${STAGING_DIR}"
 }
 
 . ${HELPERSDIR}/call_functions.sh
