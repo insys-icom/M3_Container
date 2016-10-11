d7b372e83ddb402d45ae44e10e42654f30d15fee
Michael Kress <m-kress@m-kress.de>
2016-10-11 20:48:05 +0200

    update container scripts of mosqitto and modbus-mqtt-gateway

--- scripts/create_container_modbus_mqtt_gateway.sh
+++ scripts/create_container_modbus_mqtt_gateway.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
-DESCRIPTOR="A container to demonstrate a modbus to mqtt gateway"
+DESCRIPTION="A container to demonstrate a modbus to MQTT gateway"
 CONTAINER_NAME="Modbus_to_MQTT_Gateway"
 ROOTFS_LIST="modbus_mqtt_gateway.txt"
 
-echo "This creates a container with a modbus to mqtt gateway in it"
+SCRIPTSDIR="$(dirname $0)"
+TOPDIR="$(realpath ${SCRIPTSDIR}/..)"
+. ${TOPDIR}/scripts/common_settings.sh
+. ${TOPDIR}/scripts/helpers.sh
+
+echo "This creates a container with a modbus to MQTT gateway in it."
+echo "Within the container will start an SSH server for logins. Both user name and password is \"root\"."
 echo ""
 echo "It is necessary to build these Open Source projects in this order:"
 echo "- Linux-PAM-1.2.1.sh"
@@ -33,35 +39,43 @@ echo " "
 echo "Continue? <y/n>"
 
 read text
-! [ "${text}" = "y" ] && exit 0
+! [ "${text}" = "y" -o "${text}" = "yes" ] && exit 0
 
-SCRIPTSDIR="$(dirname $0)"
-TOPDIR="$(realpath ${SCRIPTSDIR}/..)"
-. ${TOPDIR}/scripts/common_settings.sh
+PACKAGES="${PACKAGES}  Linux-PAM-1.2.1.sh"
+PACKAGES="${PACKAGES}  busybox-1.24.2.sh"
+PACKAGES="${PACKAGES}  finit-1.10.sh"
+PACKAGES="${PACKAGES}  zlib-1.2.8.sh"
+PACKAGES="${PACKAGES}  dropbear-2016.73.sh"
+PACKAGES="${PACKAGES}  timezone2016e.sh"
+PACKAGES="${PACKAGES}  mcip.sh"
+PACKAGES="${PACKAGES}  pcre-8.38.sh"
+PACKAGES="${PACKAGES}  metalog-3.sh"
+PACKAGES="${PACKAGES}  openssl-1.0.2h.sh"
+PACKAGES="${PACKAGES}  libxml2-2.9.4.sh"
+PACKAGES="${PACKAGES}  sqlite-src-3110100.sh"
+PACKAGES="${PACKAGES}  gdbm-1.12.sh"
+PACKAGES="${PACKAGES}  lighttpd-1.4.39.sh"
+PACKAGES="${PACKAGES}  web_interface_modbus_to_mqtt_gateway-0.1.sh"
+PACKAGES="${PACKAGES}  app_handler-0.1.sh"
+PACKAGES="${PACKAGES}  c-ares-1.12.0.sh"
+PACKAGES="${PACKAGES}  libmodbus-3.1.4.sh"
+PACKAGES="${PACKAGES}  mosquitto-1.4.10.sh"
+PACKAGES="${PACKAGES}  modbus_to_mqtt_gateway-0.1.sh"
 
 # compile the needed packages
-${OSS_PACKAGES_SCRIPTS}/Linux-PAM-1.2.1.sh all
-${OSS_PACKAGES_SCRIPTS}/busybox-1.24.2.sh all
-${OSS_PACKAGES_SCRIPTS}/finit-1.10.sh all
-${OSS_PACKAGES_SCRIPTS}/zlib-1.2.8.sh all
-${OSS_PACKAGES_SCRIPTS}/dropbear-2016.73.sh all
-${OSS_PACKAGES_SCRIPTS}/timezone2016e.sh all
-${OSS_PACKAGES_SCRIPTS}/mcip.sh all
-${OSS_PACKAGES_SCRIPTS}/pcre-8.38.sh all
-${OSS_PACKAGES_SCRIPTS}/metalog-3.sh all
-${OSS_PACKAGES_SCRIPTS}/openssl-1.0.2h.sh all
-${OSS_PACKAGES_SCRIPTS}/libxml2-2.9.4.sh all
-${OSS_PACKAGES_SCRIPTS}/sqlite-src-3110100.sh all
-${OSS_PACKAGES_SCRIPTS}/gdbm-1.12.sh all
-${OSS_PACKAGES_SCRIPTS}/lighttpd-1.4.39.sh all
-${OSS_PACKAGES_SCRIPTS}/web_interface_modbus_to_mqtt_gateway-0.1.sh all
-${OSS_PACKAGES_SCRIPTS}/app_handler-0.1.sh all
-${OSS_PACKAGES_SCRIPTS}/c-ares-1.12.0.sh all
-${OSS_PACKAGES_SCRIPTS}/libmodbus-3.1.4.sh all
-${OSS_PACKAGES_SCRIPTS}/mosquitto-1.4.10.sh all
-${OSS_PACKAGES_SCRIPTS}/modbus_to_mqtt_gateway-0.1.sh all
+for PACKAGE in ${PACKAGES} ; do
+    echo ""
+    echo "*************************************************************************************"
+    echo "* downloading, checking, configuring, compiling and installing ${PACKAGE%.sh}"
+    echo "*************************************************************************************"
+    echo ""
+    ${OSS_PACKAGES_SCRIPTS}/${PACKAGE}          all || exit
+done
 
 # package container
-echo " "
-echo "Packaging the container"
+echo ""
+echo "*************************************************************************************"
+echo "* Packaging the container"
+echo "*************************************************************************************"
+echo ""
 ${TOPDIR}/scripts/mk_container.sh -n "${CONTAINER_NAME}" -l "${ROOTFS_LIST}" -d "${DESCRIPTION}" -v "1.0"
--- scripts/create_container_mosquitto_mqtt_broker.sh
+++ scripts/create_container_mosquitto_mqtt_broker.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
-DESCRIPTOR="A container to demonstrate a mosquitto mqtt broker"
+DESCRIPTION="A container to demonstrate the mosquitto MQTT broker"
 CONTAINER_NAME="Mosquitto_MQTT_Broker"
 ROOTFS_LIST="mosquitto_mqtt_broker.txt"
 
-echo "This creates a container with a mosquitto mqtt broker in it"
+SCRIPTSDIR="$(dirname $0)"
+TOPDIR="$(realpath ${SCRIPTSDIR}/..)"
+. ${TOPDIR}/scripts/common_settings.sh
+. ${TOPDIR}/scripts/helpers.sh
+
+echo "This creates a container with the mosquitto MQTT broker in it."
+echo "Within the container will start an SSH server for logins. Both user name and password is \"root\"."
 echo ""
 echo "It is necessary to build these Open Source projects in this order:"
 echo "- Linux-PAM-1.2.1.sh"
@@ -20,11 +26,11 @@ echo "- libxml2-2.9.4.sh"
 echo "- sqlite-src-3110100.sh"
 echo "- gdbm-1.12.sh"
 echo "- lighttpd-1.4.39.sh"
-echo "- c-ares-1.12.0.sh"	
-echo "- web_interface_mosquitto_mqtt_broker-0.1.sh"		
-echo "- app_handler-0.1.sh"								
+echo "- c-ares-1.12.0.sh"
+echo "- web_interface_mosquitto_mqtt_broker-0.1.sh"
+echo "- app_handler-0.1.sh"
 echo "- libmodbus-3.1.4.sh"
-echo "- mosquitto-1.4.10.sh"							
+echo "- mosquitto-1.4.10.sh"
 echo " "
 echo "These packages only have to be compiled once. After that you can package the container yourself with"
 echo " # ./scripts/mk_container.sh -n ${CONTAINER_NAME} -l ${ROOTFS_LIST}"
@@ -32,34 +38,46 @@ echo " "
 echo "Continue? <y/n>"
 
 read text
-! [ "${text}" = "y" ] && exit 0
+! [ "${text}" = "y" -o "${text}" = "yes" ] && exit 0
 
 SCRIPTSDIR="$(dirname $0)"
 TOPDIR="$(realpath ${SCRIPTSDIR}/..)"
 . ${TOPDIR}/scripts/common_settings.sh
 
+PACKAGES="${PACKAGES}  Linux-PAM-1.2.1.sh"
+PACKAGES="${PACKAGES}  busybox-1.24.2.sh"
+PACKAGES="${PACKAGES}  finit-1.10.sh"
+PACKAGES="${PACKAGES}  zlib-1.2.8.sh"
+PACKAGES="${PACKAGES}  dropbear-2016.73.sh"
+PACKAGES="${PACKAGES}  timezone2016e.sh"
+PACKAGES="${PACKAGES}  mcip.sh"
+PACKAGES="${PACKAGES}  pcre-8.38.sh"
+PACKAGES="${PACKAGES}  metalog-3.sh"
+PACKAGES="${PACKAGES}  openssl-1.0.2h.sh"
+PACKAGES="${PACKAGES}  libxml2-2.9.4.sh"
+PACKAGES="${PACKAGES}  sqlite-src-3110100.sh"
+PACKAGES="${PACKAGES}  gdbm-1.12.sh"
+PACKAGES="${PACKAGES}  lighttpd-1.4.39.sh"
+PACKAGES="${PACKAGES}  web_interface_mosquitto_mqtt_broker-0.1.sh"
+PACKAGES="${PACKAGES}  app_handler-0.1.sh"
+PACKAGES="${PACKAGES}  c-ares-1.12.0.sh"
+PACKAGES="${PACKAGES}  libmodbus-3.1.4.sh"
+PACKAGES="${PACKAGES}  mosquitto-1.4.10.sh"
+
 # compile the needed packages
-${OSS_PACKAGES_SCRIPTS}/Linux-PAM-1.2.1.sh all
-${OSS_PACKAGES_SCRIPTS}/busybox-1.24.2.sh all
-${OSS_PACKAGES_SCRIPTS}/finit-1.10.sh all
-${OSS_PACKAGES_SCRIPTS}/zlib-1.2.8.sh all
-${OSS_PACKAGES_SCRIPTS}/dropbear-2016.73.sh all
-${OSS_PACKAGES_SCRIPTS}/timezone2016e.sh all
-${OSS_PACKAGES_SCRIPTS}/mcip.sh all
-${OSS_PACKAGES_SCRIPTS}/pcre-8.38.sh all
-${OSS_PACKAGES_SCRIPTS}/metalog-3.sh all
-${OSS_PACKAGES_SCRIPTS}/openssl-1.0.2h.sh all
-${OSS_PACKAGES_SCRIPTS}/libxml2-2.9.4.sh all
-${OSS_PACKAGES_SCRIPTS}/sqlite-src-3110100.sh all
-${OSS_PACKAGES_SCRIPTS}/gdbm-1.12.sh all
-${OSS_PACKAGES_SCRIPTS}/lighttpd-1.4.39.sh all
-${OSS_PACKAGES_SCRIPTS}/web_interface_mosquitto_mqtt_broker-0.1.sh all
-${OSS_PACKAGES_SCRIPTS}/app_handler-0.1.sh all
-${OSS_PACKAGES_SCRIPTS}/c-ares-1.12.0.sh all	
-${OSS_PACKAGES_SCRIPTS}/libmodbus-3.1.4.sh all
-${OSS_PACKAGES_SCRIPTS}/mosquitto-1.4.10.sh all
+for PACKAGE in ${PACKAGES} ; do
+    echo ""
+    echo "*************************************************************************************"
+    echo "* downloading, checking, configuring, compiling and installing ${PACKAGE%.sh}"
+    echo "*************************************************************************************"
+    echo ""
+    ${OSS_PACKAGES_SCRIPTS}/${PACKAGE}          all || exit
+done
 
 # package container
-echo " "
-echo "Packaging the container"
+echo ""
+echo "*************************************************************************************"
+echo "* Packaging the container"
+echo "*************************************************************************************"
+echo ""
 ${TOPDIR}/scripts/mk_container.sh -n "${CONTAINER_NAME}" -l "${ROOTFS_LIST}" -d "${DESCRIPTION}" -v "1.0"
