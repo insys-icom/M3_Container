c094e3bb5ee926274afc41e5fcae919e44efea60
Michael Kress <m-kress@m-kress.de>
2017-01-15 10:19:35 +0100

    added unmodified setup files for Python 3.6

--- oss_packages/scripts/python-3.6.0.sh
+++ oss_packages/scripts/python-3.6.0.sh
@@ -26,13 +26,24 @@ PKG_INSTALL_DIR="${PKG_BUILD_DIR}/install"
 
 configure()
 {
+    echo "Be sure you compiled these oss_packages first:"
+    echo " - openssl"
+    echo " - zlib"
+    echo " - expat"
+    echo " - sqlite"
+    echo " "
+    echo "Continue? <y/n>"
+
+    read text
+    ! [ "${text}" = "y" -o "${text}" = "yes" ] && exit 0
+
     # compile Python for x86
     PYTHON_HOST="${PKG_BUILD_DIR}/Python_host"
     mkdir -P "${PYTHON_HOST}"
     cd "${PKG_BUILD_DIR}"
     ./configure --prefix="${PKG_BUILD_DIR}/Python_host" --with-fpectl --with-computed-gotos --with-libc= --without-ensurepip --without-lto
 
-    make
+    make ${M3_MAKEFLAGS}
     make install
 
     rm "${PKG_BUILD_DIR}/Parser/"*".o"
@@ -41,25 +52,29 @@ configure()
     # compile Python for target
     copy_overlay
     cd "${PKG_BUILD_DIR}"
+    make distclean
+
     export PYTHONHOME="${PYTHON_HOST}"
     export PATH="${PATH}:${PYTHON_HOST}/bin"
-    export CFLAGS="${M3_CFLAGS} -I${STAGING_INCLUDE} -I${STAGING_DIR}/usr/include/ncurses -I${STAGING_DIR}/usr/include/ncursesw"
+    export CFLAGS="${M3_CFLAGS} -I${STAGING_INCLUDE}"
     export LDFLAGS="${M3_LDFLAGS} -L${STAGING_LIB}"
     export ac_cv_file__dev_ptc=no
     export ac_cv_file__dev_ptmx=no
     export CXX
 
-    ./configure --target=${M3_TARGET} --host=${M3_TARGET} --build=i686-pc-linux-gnu --with-fpectl --with-libc= --without-ensurepip --enable-ipv6 --with-threads --with-computed-gotos --with-system-expat --with-lto --enable-shared
-
-    #sed -e 's,\$(\([A-Za-z0-9_]*\)),\$\{\1\},g' < Misc/python-config.sh >python-config
+    ./configure --target=${M3_TARGET} --host=${M3_TARGET} --build=i686-pc-linux-gnu --with-fpectl --enable-ipv6 --with-threads --with-computed-gotos --with-system-expat --with-lto --enable-shared --enable-loadable-sqlite-extensions --without-ensurepip
 }
 
 compile()
 {
+    export PYTHON_HOST="${PKG_BUILD_DIR}/Python_host"
     export PYTHONHOME="${PYTHON_HOST}"
     export PYTHONPATH="${PYTHON_HOST}/lib/python3.6"
     export PATH="${PATH}:${PYTHON_HOST}/bin"
     PYTHON_FOR_BUILD="${PYTHON_HOST}/bin/python3.6"
+    export STAGING_INCLUDE="${STAGING_INCLUDE}"
+    export STAGING_LIB="${STAGING_LIB}"
+
     cd "${PKG_BUILD_DIR}"
     make ${M3_MAKEFLAGS} || exit_failure "failed to build ${PKG_DIR}"
     make DESTDIR="${PKG_INSTALL_DIR}" install
@@ -67,6 +82,12 @@ compile()
 
 install_staging()
 {
+    export PYTHON_HOST="${PKG_BUILD_DIR}/Python_host"
+    export PYTHONHOME="${PYTHON_HOST}"
+    export PYTHONPATH="${PYTHON_HOST}/lib/python3.6"
+    export PATH="${PATH}:${PYTHON_HOST}/bin"
+    PYTHON_FOR_BUILD="${PYTHON_HOST}/bin/python3.6"
+
     cd "${PKG_BUILD_DIR}"
     make -i DESTDIR="${STAGING_DIR}" install || exit_failure "failed to install ${PKG_DIR}"
 
