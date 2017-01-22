2f91d82bbf4da76d02ce4b2a23a5c6aab7f7f809
Michael Kress <m-kress@m-kress.de>
2017-01-22 18:44:22 +0100

    enable more features in sqlite3 build script

--- oss_packages/scripts/sqlite-src-3110100.sh
+++ oss_packages/scripts/sqlite-src-3110100.sh
@@ -29,76 +29,77 @@ PKG_INSTALL_DIR="${PKG_BUILD_DIR}/install"
 # see https://www.sqlite.org/compile.html for an explanation of the flags
 # https://www.sqlite.org/limits.html
 
-SQLITE3_OMIT_FLAGS=" -DSQLITE_TEMP_STORE=2 \
-                     -DSQLITE_DEFAULT_CACHE_SIZE=500 \
-                     -DSQLITE_DEFAULT_WAL_AUTOCHECKPOINT=250 \
-                     -DSQLITE_DEFAULT_JOURNAL_SIZE_LIMIT=65536 \
-                     -DSQLITE_MAX_LENGTH=100000 \
-                     -DSQLITE_MAX_COLUMN=32 \
-                     -DSQLITE_MAX_SQL_LENGTH=10000 \
-                     -DSQLITE_MAX_VARIABLE_NUMBER=32 \
-\
--USQLITE_OMIT_ALTERTABLE \
-                                -DSQLITE_OMIT_ANALYZE \
--USQLITE_OMIT_ATTACH \
-                                -DSQLITE_OMIT_AUTHORIZATION \
--USQLITE_OMIT_AUTOINCREMENT \
--USQLITE_OMIT_AUTOINIT \
--USQLITE_OMIT_AUTOMATIC_INDEX \
--USQLITE_OMIT_AUTORESET \
--USQLITE_OMIT_AUTOVACUUM \
--USQLITE_OMIT_BETWEEN_OPTIMIZATION \
--USQLITE_OMIT_BLOB_LITERAL \
-                                -DSQLITE_OMIT_BTREECOUNT \
--USQLITE_OMIT_BUILTIN_TEST \
-                                -DSQLITE_OMIT_CAST \
-                                -DSQLITE_OMIT_CHECK \
-                                -DSQLITE_OMIT_COMPILEOPTION_DIAGS \
--USQLITE_OMIT_COMPLETE \
--USQLITE_OMIT_COMPOUND_SELECT \
-                                -DSQLITE_OMIT_CTE \
-                                -DSQLITE_OMIT_DATETIME_FUNCS \
-                                -DSQLITE_OMIT_DECLTYPE \
-                                -DSQLITE_OMIT_DEPRECATED \
--USQLITE_OMIT_DISKIO \
--USQLITE_OMIT_EXPLAIN \
--USQLITE_OMIT_FLAG_PRAGMAS \
--USQLITE_OMIT_FLOATING_POINT \
--USQLITE_OMIT_FOREIGN_KEY \
--USQLITE_OMIT_GET_TABLE \
-                                -DSQLITE_OMIT_INCRBLOB \
--USQLITE_OMIT_INTEGRITY_CHECK \
--USQLITE_OMIT_LIKE_OPTIMIZATION \
-                                -DSQLITE_OMIT_LOAD_EXTENSION \
-                                -DSQLITE_OMIT_LOCALTIME \
--USQLITE_OMIT_LOOKASIDE \
--USQLITE_OMIT_MEMORYDB \
--USQLITE_OMIT_OR_OPTIMIZATION \
--USQLITE_OMIT_PAGER_PRAGMAS \
--USQLITE_OMIT_PRAGMA \
-                                -DSQLITE_OMIT_PROGRESS_CALLBACK \
--USQLITE_OMIT_QUICKBALANCE \
-                                -DSQLITE_OMIT_REINDEX \
-                                -DSQLITE_OMIT_SCHEMA_PRAGMAS \
-                                -DSQLITE_OMIT_SCHEMA_VERSION_PRAGMAS \
-                                -DSQLITE_OMIT_SHARED_CACHE \
--USQLITE_OMIT_SUBQUERY \
-                                -DSQLITE_OMIT_TCL_VARIABLE \
--USQLITE_OMIT_TEMPDB \
-                                -DSQLITE_OMIT_TRACE \
-                                -DSQLITE_OMIT_TRIGGER \
-                                -DSQLITE_OMIT_TRUNCATE_OPTIMIZATION \
--USQLITE_OMIT_UTF16 \
--USQLITE_OMIT_VACUUM \
-                                -DSQLITE_OMIT_VIEW \
-                                -DSQLITE_OMIT_VIRTUALTABLE \
--USQLITE_OMIT_WAL \
--USQLITE_OMIT_WSD \
-                                -DSQLITE_OMIT_XFER_OPT \
--USQLITE_ZERO_MALLOC \
--USQLITE_ENABLE_API_ARMOR \
--USQLITE_DEBUG \
-"
+#SQLITE3_OMIT_FLAGS=" -DSQLITE_TEMP_STORE=2 \
+#                     -DSQLITE_DEFAULT_CACHE_SIZE=500 \
+#                     -DSQLITE_DEFAULT_WAL_AUTOCHECKPOINT=250 \
+#                     -DSQLITE_DEFAULT_JOURNAL_SIZE_LIMIT=65536 \
+#                     -DSQLITE_MAX_LENGTH=100000 \
+#                     -DSQLITE_MAX_COLUMN=32 \
+#                     -DSQLITE_MAX_SQL_LENGTH=10000 \
+#                     -DSQLITE_MAX_VARIABLE_NUMBER=32 \
+#                       \
+#                     -USQLITE_OMIT_ALTERTABLE \
+#                     -DSQLITE_OMIT_ANALYZE \
+#                     -USQLITE_OMIT_ATTACH \
+#                     -DSQLITE_OMIT_AUTHORIZATION \
+#                     -USQLITE_OMIT_AUTOINCREMENT \
+#                     -USQLITE_OMIT_AUTOINIT \
+#                     -USQLITE_OMIT_AUTOMATIC_INDEX \
+#                     -USQLITE_OMIT_AUTORESET \
+#                     -USQLITE_OMIT_AUTOVACUUM \
+#                     -USQLITE_OMIT_BETWEEN_OPTIMIZATION \
+#                     -USQLITE_OMIT_BLOB_LITERAL \
+#                     -DSQLITE_OMIT_BTREECOUNT \
+#                     -USQLITE_OMIT_BUILTIN_TEST \
+#                     -DSQLITE_OMIT_CAST \
+#                     -DSQLITE_OMIT_CHECK \
+#                     -DSQLITE_OMIT_COMPILEOPTION_DIAGS \
+#                     -USQLITE_OMIT_COMPLETE \
+#                     -USQLITE_OMIT_COMPOUND_SELECT \
+#                     -DSQLITE_OMIT_CTE \
+#                     -DSQLITE_OMIT_DATETIME_FUNCS \
+#                     -DSQLITE_OMIT_DECLTYPE \
+#                     -DSQLITE_OMIT_DEPRECATED \
+#                     -USQLITE_OMIT_DISKIO \
+#                     -USQLITE_OMIT_EXPLAIN \
+#                     -USQLITE_OMIT_FLAG_PRAGMAS \
+#                     -USQLITE_OMIT_FLOATING_POINT \
+#                     -USQLITE_OMIT_FOREIGN_KEY \
+#                     -USQLITE_OMIT_GET_TABLE \
+#                     -DSQLITE_OMIT_INCRBLOB \
+#                     -USQLITE_OMIT_INTEGRITY_CHECK \
+#                     -USQLITE_OMIT_LIKE_OPTIMIZATION \
+#                     -DSQLITE_OMIT_LOAD_EXTENSION \
+#                     -DSQLITE_OMIT_LOCALTIME \
+#                     -USQLITE_OMIT_LOOKASIDE \
+#                     -USQLITE_OMIT_MEMORYDB \
+#                     -USQLITE_OMIT_OR_OPTIMIZATION \
+#                     -USQLITE_OMIT_PAGER_PRAGMAS \
+#                     -USQLITE_OMIT_PRAGMA \
+#                     -DSQLITE_OMIT_PROGRESS_CALLBACK \
+#                     -USQLITE_OMIT_QUICKBALANCE \
+#                     -DSQLITE_OMIT_REINDEX \
+#                     -DSQLITE_OMIT_SCHEMA_PRAGMAS \
+#                     -DSQLITE_OMIT_SCHEMA_VERSION_PRAGMAS \
+#                     -DSQLITE_OMIT_SHARED_CACHE \
+#                     -USQLITE_OMIT_SUBQUERY \
+#                     -DSQLITE_OMIT_TCL_VARIABLE \
+#                     -USQLITE_OMIT_TEMPDB \
+#                     -DSQLITE_OMIT_TRACE \
+#                     -DSQLITE_OMIT_TRIGGER \
+#                     -DSQLITE_OMIT_TRUNCATE_OPTIMIZATION \
+#                     -USQLITE_OMIT_UTF16 \
+#                     -USQLITE_OMIT_VACUUM \
+#                     -DSQLITE_OMIT_VIEW \
+#                     -DSQLITE_OMIT_VIRTUALTABLE \
+#                     -USQLITE_OMIT_WAL \
+#                     -USQLITE_OMIT_WSD \
+#                     -DSQLITE_OMIT_XFER_OPT \
+#                     -USQLITE_ZERO_MALLOC \
+#                     -USQLITE_ENABLE_API_ARMOR \
+#                     -USQLITE_DEBUG \
+#                     "
+
 # API_ARMOR and SQLITE_DEBUG is for development only
 
 unpack()
