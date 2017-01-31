7dedc933e874b24f3a0fa9f8ee4148671f4e0a55
Michael Kress <m-kress@m-kress.de>
2017-01-31 17:15:20 +0100

    clean env to make node.js build all work

--- oss_packages/scripts/node-v7.4.0.sh
+++ oss_packages/scripts/node-v7.4.0.sh
@@ -27,44 +27,16 @@ PKG_INSTALL_DIR="${PKG_BUILD_DIR}/install"
 configure()
 {
     cd "${PKG_BUILD_DIR}"
-    #export CROSS_COMPILE="${M3_CROSS_COMPILE}"
     export CFLAGS="${M3_CFLAGS} -L${STAGING_LIB} -I${STAGING_INCLUDE}"
     export LDFLAGS="${M3_LDFLAGS} -L${STAGING_LIB}"
-
-    #Define our target device
     export TARGET_ARCH="-march=armv7-a"
-    export TARGET_TUNE="-mtune=cortex-a8 -mfpu=neon -mfloat-abi=softfp -mthumb-interwork -mno-thumb"
-
-    #Define the cross compilators on your system
-    export AR="armv7a-hardfloat-linux-gnueabi-ar"
+    export TARGET_TUNE="-mtune=cortex-a8 -mfpu=neon -mfloat-abi=hardfp -mthumb-interwork -mno-thumb"
     export CC="armv7a-hardfloat-linux-gnueabi-gcc"
     export CXX="armv7a-hardfloat-linux-gnueabi-g++"
-    export LINK="armv7a-hardfloat-linux-gnueabi-g++"
     export CPP="armv7a-hardfloat-linux-gnueabi-gcc -E"
     export LD="armv7a-hardfloat-linux-gnueabi-ld"
-    export AS="armv7a-hardfloat-linux-gnueabi-as"
-    export CCLD="armv7a-hardfloat-linux-gnueabi-gcc ${TARGET_ARCH} ${TARGET_TUNE}"
-    export NM="armv7a-hardfloat-linux-gnueabi-nm"
-    export STRIP="armv7a-hardfloat-linux-gnueabi-strip"
-    export OBJCOPY="armv7a-hardfloat-linux-gnueabi-objcopy"
-    export RANLIB="armv7a-hardfloat-linux-gnueabi-ranlib"
-    export F77="armv7a-hardfloat-linux-gnueabi-g77 ${TARGET_ARCH} ${TARGET_TUNE}"
-
-    export AR_host="ar"
     export CC_host="gcc"
     export CXX_host="g++"
-    export LINK_host="g++"
-    export CPP_host="gcc -E"
-    export LD_host="ld"
-    export AS_host="as"
-    export CCLD_host="gcc"
-    export NM_host="nm"
-    export STRIP_host="strip"
-    export OBJCOPY_host="objcopy"
-    export RANLIB_host="ranlib"
-    export F77_host="g77"
-
-    unset LIBC
 
     ./configure --without-snapshot --prefix="/" --dest-cpu=arm --dest-os=linux --cross-compiling \
         --shared-openssl --shared-openssl-includes="${STAGING_INCLUDE}" --shared-openssl-libpath="${STAGING_LIB}" \
@@ -88,13 +60,23 @@ configure()
         sed -i 's/-lssl \\/\\/g' "${FILE}"
         sed -i 's/-lssl//g' "${FILE}"
     done
-
 }
 
 compile()
 {
     copy_overlay
     cd "${PKG_BUILD_DIR}"
+    unset CFLAGS
+    unset LDFLAGS
+    unset TARGET_ARCH
+    unset TARGET_TUNE
+    unset CC
+    unset CXX
+    unset CPP
+    unset LD
+    unset CC_host
+    unset CXX_host
+
     make "${M3_MAKEFLAGS}" || exit_failure "failed to build ${PKG_DIR}"
     make DESTDIR="${PKG_INSTALL_DIR}" install
 }
