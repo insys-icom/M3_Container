e723cf657c35f230b89394623251bf8f7d2a3101
Michael Kress <m-kress@m-kress.de>
2017-01-31 12:59:44 +0100

    added original Makefile for http-paraser

---
+++ oss_packages/scripts/http-parser-v2.7.1.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+
+# download link for the sources to be stored in dl directory
+PKG_DOWNLOAD="https://github.com/nodejs/http-parser/archive/v2.7.1.tar.gz"
+
+# md5 checksum of archive in dl directory
+PKG_CHECKSUM="31c6fefb6208b16d19b3f990e71cb04a"
+
+# name of directory after extracting the archive in working directory
+PKG_DIR="http-parser-2.7.1"
+
+# name of the archive in dl directory
+PKG_ARCHIVE_FILE="${PKG_DIR}.tar.gz"
+
+SCRIPTSDIR="$(dirname $0)"
+HELPERSDIR="${SCRIPTSDIR}/helpers"
+TOPDIR="$(realpath ${SCRIPTSDIR}/../..)"
+
+. ${TOPDIR}/scripts/common_settings.sh
+. ${HELPERSDIR}/functions.sh
+
+PKG_ARCHIVE="${DOWNLOADS_DIR}/${PKG_ARCHIVE_FILE}"
+PKG_SRC_DIR="${SOURCES_DIR}/${PKG_DIR}"
+PKG_BUILD_DIR="${BUILD_DIR}/${PKG_DIR}"
+PKG_INSTALL_DIR="${PKG_BUILD_DIR}/install"
+
+configure()
+{
+    true
+}
+
+compile()
+{
+    copy_overlay
+    cd "${PKG_BUILD_DIR}"
+    export CFLAGS="${M3_CFLAGS} -L${STAGING_LIB} -I${STAGING_INCLUDE}"
+    export CPPFLAGS="${M3_CFLAGS} -L${STAGING_LIB} -I${STAGING_INCLUDE}"
+    export LDFLAGS="${M3_LDFLAGS} -L${STAGING_LIB}"
+    export AR="armv7a-hardfloat-linux-gnueabi-ar"
+    export CC="armv7a-hardfloat-linux-gnueabi-gcc"
+    export PREFIX='$(DESTDIR)'
+    make "${M3_MAKEFLAGS}" || exit_failure "failed to build ${PKG_DIR}"
+    make DESTDIR="${PKG_INSTALL_DIR}" install
+}
+
+install_staging()
+{
+    cd "${PKG_BUILD_DIR}"
+    export CFLAGS="${M3_CFLAGS} -L${STAGING_LIB} -I${STAGING_INCLUDE}"
+    export CPPFLAGS="${M3_CFLAGS} -L${STAGING_LIB} -I${STAGING_INCLUDE}"
+    export LDFLAGS="${M3_LDFLAGS} -L${STAGING_LIB}"
+    export AR="armv7a-hardfloat-linux-gnueabi-ar"
+    export CC="armv7a-hardfloat-linux-gnueabi-gcc"
+    export PREFIX='$(DESTDIR)'
+    make -i DESTDIR="${STAGING_DIR}" install || exit_failure "failed to install ${PKG_DIR}"
+}
+
+. ${HELPERSDIR}/call_functions.sh
