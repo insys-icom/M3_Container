f4a175a7f0e044023c07d7ad2d7963e8708405ec
Michael Kress <m-kress@m-kress.de>
2017-03-24 15:41:07 +0100

    added wget build script

---
+++ oss_packages/scripts/wget-1.19.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+# download link for the sources to be stored in dl directory
+PKG_DOWNLOAD="http://ftp.gnu.org/gnu/wget/wget-1.19.tar.xz"
+
+# md5 checksum of archive in dl directory
+PKG_CHECKSUM="1814393c5955a6148ff6d82c4a9e3c21"
+
+# name of directory after extracting the archive in working directory
+PKG_DIR="wget-1.19"
+
+# name of the archive in dl directory
+PKG_ARCHIVE_FILE="${PKG_DIR}.tar.gz"
+
+SCRIPTSDIR="$(dirname $0)"
+HELPERSDIR="${SCRIPTSDIR}/helpers"
+TOPDIR="$(realpath ${SCRIPTSDIR}/../..)"
+
+. ${TOPDIR}/scripts/common_settings.sh
+. ${HELPERSDIR}/functions.sh
+
+PKG_ARCHIVE="${DOWNLOADS_DIR}/${PKG_ARCHIVE_FILE}"
+PKG_SRC_DIR="${SOURCES_DIR}/${PKG_DIR}"
+PKG_BUILD_DIR="${BUILD_DIR}/${PKG_DIR}"
+PKG_INSTALL_DIR="${PKG_BUILD_DIR}/install"
+
+configure()
+{
+    cd "${PKG_BUILD_DIR}"
+    #export CROSS_COMPILE="${M3_CROSS_COMPILE}"
+    export CFLAGS="${M3_CFLAGS} -L${STAGING_LIB} -I${STAGING_INCLUDE}"
+    export LDFLAGS="${M3_LDFLAGS} -L${STAGING_LIB}"
+    #export OPENSSL_LIBS="${STAGING_LIB}"
+    #export OPENSSL_CFLAGS="${STAGING_INCLUDE}"
+    ./configure --target="${M3_TARGET}" --host="${M3_TARGET}" --prefix="" --disable-nls --disable-largefile --with-ssl=openssl --with-libssl-prefix="${STAGING_DIR}"
+}
+
+compile()
+{
+    copy_overlay
+    cd "${PKG_BUILD_DIR}"
+    make "${M3_MAKEFLAGS}" || exit_failure "failed to build ${PKG_DIR}"
+    make DESTDIR="${PKG_INSTALL_DIR}" install
+}
+
+install_staging()
+{
+    cd "${PKG_BUILD_DIR}"
+    make -i DESTDIR="${STAGING_DIR}" install || exit_failure "failed to install ${PKG_DIR}"
+}
+
+. ${HELPERSDIR}/call_functions.sh
