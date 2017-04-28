b5587ce06eda2defc0a52f07d5a90e56b554db1b
Michael Kress <m-kress@m-kress.de>
2017-04-28 14:32:56 +0200

    added mk_all.sh, that builds every single OSS package and creates all containers for which a cooking recipe exists

---
+++ scripts/mk_all.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+
+# print error message and exit
+exit_message()
+{
+    echo $*
+    exit 1
+}
+
+echo "This script will "
+echo " - erase already created containers in \"images\""
+echo " - erase everything in the directories \"rootfs_staging\" and \"working\""
+echo " - erase all downloaded open source packets in \"oss_packages/dl\""
+echo " - compile all open source projects given in the file \"oss_packages/scripts/helpers/list.txt\""
+echo " - create all containers for witch cooking recipes exist (\"/scripts/create_container_XXX.sh\")"
+echo " "
+echo "Continue? <y/n>"
+
+read text
+! [ "${text}" = "y" -o "${text}" = "yes" ] && exit 0
+
+SCRIPTSDIR="$(dirname $0)"
+TOPDIR="$(realpath ${SCRIPTSDIR}/..)"
+. ${TOPDIR}/scripts/common_settings.sh
+LIST_FILE="${OSS_PACKAGES_SCRIPTS}/helpers/list.txt"
+
+# clean up
+#rm -Rf "${TOPDIR}/images/"*
+#rm -Rf "${STAGING_DIR}/"*
+#rm -Rf "${TOPDIR}/working/"*
+#rm -Rf "${DOWNLOADS_DIR}/"*
+#
+### build all open source projects in the order given by the list file
+#while read BUILDSCRIPT ; do
+#    # ignore line with starting # or space
+#    [ "$(echo $BUILDSCRIPT | cut -c1-1)" = '#' -o "$(echo $BUILDSCRIPT | cut -c1-1)" = "" ] && continue
+#
+#    # announce the build script
+#    echo ""
+#    echo "###########################################################################"
+#    printf "executing \"%s$BUILDSCRIPT all\"\n"
+#    echo "###########################################################################"
+#
+#    # process build script
+#    "${OSS_PACKAGES_SCRIPTS}/${BUILDSCRIPT}" all || exit_message "Failed to execute ${BUILDSCRIPT} all"
+#done < "$(ls ${LIST_FILE})"
+
+# create all containers
+cd "${SCRIPTSDIR}/rootfs_lists"
+for LIST in *.txt ; do
+    NAME="container_$(echo $LIST | cut -d'.' -f1)"
+    DATE="$(date +\"%F\")"
+    "${TOPDIR}"/scripts/mk_container.sh -n "${NAME}" -l "${LIST}" -v "${DATE}" || exit_message "Failed to create ${NAME}.tar"
+done
+
+# upload all created containers
+
+# upload all open source project archives
