fc23c512226b22c6c9b30dc7293947120fdaacd3
Michael Schindler <mschindler@insys-icom.de>
2017-06-22 14:40:52 +0200

    libraries now downloaded from webspace

---
+++ oss_packages/scripts/libmrx_control.sh
@@ -0,0 +1,45 @@
+#! /bin/sh
+
+# download link for the sources to be stored in dl directory
+PKG_DOWNLOAD="https://m3-container.net/M3_Container/closed_sources/libmrx_control-1.0.tar.gz"
+
+# md5 checksum of archive in dl directory
+PKG_CHECKSUM="1aebcd4010fcb0cf277df5e451ea22d7"
+
+# name of directory after extracting the archive in working directory
+PKG_DIR="libmrx_control-1.0"
+
+# name of the archive in dl directory
+PKG_ARCHIVE_FILE="${PKG_DIR}.tar.gz"
+
+SCRIPTSDIR="$(dirname $0)"
+HELPERSDIR="${SCRIPTSDIR}/helpers"
+TOPDIR="$(realpath ${SCRIPTSDIR}/../..)"
+
+. ${TOPDIR}/scripts/common_settings.sh
+. ${HELPERSDIR}/functions.sh
+
+PKG_ARCHIVE="${DOWNLOADS_DIR}/${PKG_ARCHIVE_FILE}"
+PKG_SRC_DIR="${SOURCES_DIR}/${PKG_DIR}"
+PKG_BUILD_DIR="${BUILD_DIR}/${PKG_DIR}"
+PKG_INSTALL_DIR="${PKG_BUILD_DIR}/install"
+
+configure()
+{
+    export CFLAGS="${M3_CFLAGS}"
+    export LDFLAGS="${M3_LDFLAGS}"
+}
+
+compile()
+{
+    :
+}
+
+install_staging()
+{
+    cd "${PKG_BUILD_DIR}"
+    cp lib/libmrx_control.so "${STAGING_DIR}/lib"
+    cp include/* "${STAGING_DIR}/include"
+}
+
+. ${HELPERSDIR}/call_functions.sh
