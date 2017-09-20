ffe4c11616d19a36586fd4b7f7e9a2b29f9e9a4b
Michael Kress <m-kress@m-kress.de>
2017-09-20 19:36:11 +0200

    added files and build scripts for minetest container

---
+++ oss_packages/scripts/irrlicht-1.8.4.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+# name of directory after extracting the archive in working directory
+PKG_DIR="irrlicht-1.8.4"
+
+# name of the archive in dl directory
+PKG_ARCHIVE_FILE="${PKG_DIR}.zip"
+
+# download link for the sources to be stored in dl directory
+# http://downloads.sourceforge.net/irrlicht/irrlicht-1.8.4.zip
+PKG_DOWNLOAD="https://m3-container.net/M3_Container/oss_packages/${PKG_ARCHIVE_FILE}"
+
+# md5 checksum of archive in dl directory
+PKG_CHECKSUM="9401cfff801395010b0912211f3cbb4f"
+
+
+
+SCRIPTSDIR="$(dirname $0)"
+HELPERSDIR="${SCRIPTSDIR}/helpers"
+TOPDIR="$(realpath ${SCRIPTSDIR}/../..)"
+. "${TOPDIR}/scripts/common_settings.sh"
+. "${HELPERSDIR}/functions.sh"
+PKG_ARCHIVE="${DOWNLOADS_DIR}/${PKG_ARCHIVE_FILE}"
+PKG_SRC_DIR="${SOURCES_DIR}/${PKG_DIR}"
+PKG_BUILD_DIR="${BUILD_DIR}/${PKG_DIR}"
+PKG_INSTALL_DIR="${PKG_BUILD_DIR}/install"
+
+configure()
+{
+    true
+}
+
+compile()
+{
+    true
+}
+
+install_staging()
+{
+    true
+}
+
+. ${HELPERSDIR}/call_functions.sh
---
+++ rootfs_skeleton/bin/minetest.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+export MINETEST_SUBGAME_PATH=/usr/share/games
+/bin/minetestserver --config /etc/minetest.conf
