35780510f459317ff52ee2b4b9c529e5b873c201
Michael Kress <m-kress@m-kress.de>
2017-09-26 14:05:14 +0200

    added new expat build script

--- oss_packages/scripts/expat-2.2.4.sh
+++ oss_packages/scripts/expat-2.2.4.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 # name of directory after extracting the archive in working directory
-PKG_DIR="expat-2.2.0"
+PKG_DIR="expat-2.2.4"
 
 # name of the archive in dl directory
 PKG_ARCHIVE_FILE="${PKG_DIR}.tar.bz2"
 
 # download link for the sources to be stored in dl directory
-# https://sourceforge.net/projects/expat/files/expat/2.2.0/expat-2.2.0.tar.bz2/download
+# https://downloads.sourceforge.net/project/expat/expat
 PKG_DOWNLOAD="https://m3-container.net/M3_Container/oss_packages/${PKG_ARCHIVE_FILE}"
 
 # md5 checksum of archive in dl directory
-PKG_CHECKSUM="2f47841c829facb346eb6e3fab5212e2"
+PKG_CHECKSUM="6e3980aba29a224a9f478d88ac7ec207"
 
 
 
@@ -28,23 +28,25 @@ PKG_INSTALL_DIR="${PKG_BUILD_DIR}/install"
 configure()
 {
     cd "${PKG_BUILD_DIR}"
-    export CFLAGS="${M3_CFLAGS} -L${STAGING_LIB} -I${STAGING_INCLUDE}"
+    export CFLAGS="${M3_CFLAGS} -I${STAGING_INCLUDE}"
     export LDFLAGS="${M3_LDFLAGS} -L${STAGING_LIB}"
-    ./configure --target=${M3_TARGET} --host=${M3_TARGET} --prefix=""
+    export PKG_CONFIG=pkg-config
+    export LIBFFI_LIBS="-lffi"
+    ./configure --target=${M3_TARGET} --host=${M3_TARGET} --prefix="" --enable-shared
 }
 
 compile()
 {
     copy_overlay
     cd "${PKG_BUILD_DIR}"
-    make ${M3_MAKEFLAGS} || exit_failure "failed to build ${PKG_DIR}"
+    make ${M3_MAKEFLAGS} V=1 || exit_failure "failed to build ${PKG_DIR}"
     make DESTDIR="${PKG_INSTALL_DIR}" install
 }
 
 install_staging()
 {
     cd "${PKG_BUILD_DIR}"
-    make -i DESTDIR="${STAGING_DIR}" install || exit_failure "failed to install ${PKG_DIR}"
+    make DESTDIR="${STAGING_DIR}" install || exit_failure "failed to install ${PKG_DIR}"
 }
 
 . ${HELPERSDIR}/call_functions.sh
