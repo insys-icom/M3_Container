025721d72c27b3765cfc8976c836f24af00901af
Michael Kress <m-kress@m-kress.de>
2017-10-16 13:08:59 +0200

    updated lz4 to version 1.8.0

--- oss_packages/scripts/lz4-1.8.0.sh
+++ oss_packages/scripts/lz4-1.8.0.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 # name of directory after extracting the archive in working directory
-PKG_DIR="lz4-1.7.5"
+PKG_DIR="lz4-1.8.0"
 
 # name of the archive in dl directory
 PKG_ARCHIVE_FILE="${PKG_DIR}.tar.gz"
 
 # download link for the sources to be stored in dl directory
-# https://github.com/lz4/lz4/archive/v1.7.5.tar.gz
+# PKG_DOWNLOAD="https://github.com/lz4/lz4/archive/v1.8.0.tar.gz"
 PKG_DOWNLOAD="https://m3-container.net/M3_Container/oss_packages/${PKG_ARCHIVE_FILE}"
 
 # md5 checksum of archive in dl directory
-PKG_CHECKSUM="c9610c5ce97eb431dddddf0073d919b9"
+PKG_CHECKSUM="6247bf0e955899969d1600ff34baed6b"
 
 
 
@@ -27,32 +27,31 @@ PKG_INSTALL_DIR="${PKG_BUILD_DIR}/install"
 
 configure()
 {
-    cd "${PKG_BUILD_DIR}"
-    export CC="${M3_CROSS_COMPILE}"gcc
-    export CFLAGS="${M3_CFLAGS} -L${STAGING_LIB} -I${STAGING_INCLUDE}"
-    export LDFLAGS="${M3_LDFLAGS} -L${STAGING_LIB}"
-    ./configure --target="${M3_TARGET}" --host="${M3_TARGET}" --prefix=""
+    true
 }
 
 compile()
 {
     copy_overlay
     cd "${PKG_BUILD_DIR}"
-    make "${M3_MAKEFLAGS}" || exit_failure "failed to build ${PKG_DIR}"
-    make DESTDIR="${PKG_INSTALL_DIR}" install
+    make DESTDIR="${PKG_INSTALL_DIR}" \
+         CC="${M3_CROSS_COMPILE}"gcc \
+         CFLAGS="${M3_CFLAGS} -L${STAGING_LIB} -I${STAGING_INCLUDE}" \
+         LDFLAGS="${M3_LDFLAGS} -L${STAGING_LIB}" \
+         install || exit_failure "failed to install ${PKG_DIR} to ${PKG_INSTALL_DIR}"
 }
 
 install_staging()
 {
     cd "${PKG_BUILD_DIR}"
-    cp "${PKG_INSTALL_DIR}/usr/local/lib/liblz4.so"* "${STAGING_DIR}/lib"
-    cp "${PKG_INSTALL_DIR}/usr/local/include/lz4.h"* "${STAGING_DIR}/include"
+    cp "${PKG_INSTALL_DIR}/usr/local/lib/liblz4.so"* "${STAGING_DIR}/lib" || exit_failure "failed to install ${PKG_DIR} to ${STAGING_DIR}"
+    cp "${PKG_INSTALL_DIR}/usr/local/include/lz4.h"* "${STAGING_DIR}/include" || exit_failure "failed to install ${PKG_DIR} to ${STAGING_DIR}"
 }
 
 uninstall_staging()
 {
-    rm -rf "${STAGING_DIR}/lib/liblz4.so"*
-    rm -rf "${STAGING_DIR}/include/lz4.h"
+    rm -vrf "${STAGING_DIR}/lib/liblz4.so"*
+    rm -vrf "${STAGING_DIR}/include/lz4.h"
 }
 
 . ${HELPERSDIR}/call_functions.sh
