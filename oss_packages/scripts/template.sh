bd31fdd02e4f9f2b781157155d318adb747f80aa
Michael Kress <m-kress@m-kress.de>
2017-10-26 10:35:25 +0200

    updated template build script

--- oss_packages/scripts/template.sh
+++ oss_packages/scripts/template.sh
@@ -7,6 +7,7 @@ PKG_DIR="project_name"
 PKG_ARCHIVE_FILE="${PKG_DIR}.tar.gz"
 
 # download link for the sources to be stored in dl directory (use "none" if empty)
+# PKG_DOWNLOAD=""
 PKG_DOWNLOAD="https://m3-container.net/M3_Container/oss_packages/${PKG_ARCHIVE_FILE}"
 
 # md5 checksum of archive in dl directory (use "none" if empty)
@@ -27,10 +28,19 @@ PKG_INSTALL_DIR="${PKG_BUILD_DIR}/install"
 configure()
 {
     cd "${PKG_BUILD_DIR}"
-    #export CROSS_COMPILE="${M3_CROSS_COMPILE}"
-    export CFLAGS="${M3_CFLAGS} -L${STAGING_LIB} -I${STAGING_INCLUDE}"
-    export LDFLAGS="${M3_LDFLAGS} -L${STAGING_LIB}"
-    ./configure --target="${M3_TARGET}" --host="${M3_TARGET}" --prefix=""
+    ./configure CROSS_COMPILE="${M3_CROSS_COMPILE}" \
+                CFLAGS="${M3_CFLAGS} -L${STAGING_LIB} -I${STAGING_INCLUDE}" \
+                LDFLAGS="${M3_LDFLAGS} -L${STAGING_LIB}" \
+                --target="${M3_TARGET}" \
+                --host="${M3_TARGET}" \
+                --prefix="" || exit_failure "failed to configure ${PKG_DIR}"
+
+    # CROSS_COMPILE="${M3_CROSS_COMPILE}" \
+    # CFLAGS="${M3_CFLAGS} -L${STAGING_LIB} -I${STAGING_INCLUDE}" \
+    # LDFLAGS="${M3_LDFLAGS} -L${STAGING_LIB}" \
+    # ./configure --target="${M3_TARGET}" \
+    #            --host="${M3_TARGET}" \
+    #            --prefix="" || exit_failure "failed to configure ${PKG_DIR}"
 }
 
 compile()
@@ -38,13 +48,18 @@ compile()
     copy_overlay
     cd "${PKG_BUILD_DIR}"
     make ${M3_MAKEFLAGS} || exit_failure "failed to build ${PKG_DIR}"
-    make DESTDIR="${PKG_INSTALL_DIR}" install
+    make DESTDIR="${PKG_INSTALL_DIR}" install || exit_failure "failed to install ${PKG_DIR} to ${PKG_INSTALL_DIR}"
 }
 
 install_staging()
 {
     cd "${PKG_BUILD_DIR}"
-    make DESTDIR="${STAGING_DIR}" install || exit_failure "failed to install ${PKG_DIR}"
+    make DESTDIR="${STAGING_DIR}" install || exit_failure "failed to install ${PKG_DIR} to ${STAGING_DIR}"
 }
+
+# uninstall_staging()
+# {
+#     rm -vf "${STAGING_DIR}/path/to/file"
+# }
 
 . ${HELPERSDIR}/call_functions.sh
