831e44bff5d9cfed05e3c0fd0a6443d4f724ef61
Michael Kress <m-kress@m-kress.de>
2017-10-26 11:19:57 +0200

    updated stunnel to version 5.42

--- oss_packages/scripts/stunnel-5.42.sh
+++ oss_packages/scripts/stunnel-5.42.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
 # name of directory after extracting the archive in working directory
-PKG_DIR="stunnel-5.41"
+PKG_DIR="stunnel-5.42"
 
 # name of the archive in dl directory
 PKG_ARCHIVE_FILE="${PKG_DIR}.tar.gz"
 
 # download link for the sources to be stored in dl directory
+# PKG_DOWNLOAD="https://www.stunnel.org/downloads/${PKG_ARCHIVE_FILE}"
 PKG_DOWNLOAD="https://m3-container.net/M3_Container/oss_packages/${PKG_ARCHIVE_FILE}"
 
 # md5 checksum of archive in dl directory
-PKG_CHECKSUM="583bb43e4344e4a7190acb636f8b5024"
+PKG_CHECKSUM="ad0a6c63b30ec40afc7a2326e9ce04e0"
 
 
 
@@ -27,9 +28,15 @@ PKG_INSTALL_DIR="${PKG_BUILD_DIR}/install"
 configure()
 {
     cd "${PKG_BUILD_DIR}"
-    export CFLAGS="${M3_CFLAGS}  -L${STAGING_LIB} -I${STAGING_INCLUDE}"
-    export LDFLAGS="${M3_LDFLAGS}  -L${STAGING_LIB}"
-    ./configure --target=${M3_TARGET} --host=${M3_TARGET} --prefix="" --disable-systemd --disable-fips --disable-libwrap --with-ssl=${STAGING_DIR}
+    ./configure CFLAGS="${M3_CFLAGS}  -L${STAGING_LIB} -I${STAGING_INCLUDE}" \
+                LDFLAGS="${M3_LDFLAGS}  -L${STAGING_LIB}" \
+                --target="${M3_TARGET}" \
+                --host="${M3_TARGET}" \
+                --prefix="" \
+                --disable-systemd \
+                --disable-fips \
+                --disable-libwrap \
+                --with-ssl="${STAGING_DIR}" || exit_failure "failed to configure ${PKG_DIR}"
 }
 
 compile()
@@ -37,7 +44,7 @@ compile()
     copy_overlay
     cd "${PKG_BUILD_DIR}"
     make ${M3_MAKEFLAGS} || exit_failure "failed to build ${PKG_DIR}"
-    make DESTDIR="${PKG_INSTALL_DIR}" install
+    make DESTDIR="${PKG_INSTALL_DIR}" install || exit_failure "failed to compile ${PKG_DIR}"
 }
 
 install_staging()
