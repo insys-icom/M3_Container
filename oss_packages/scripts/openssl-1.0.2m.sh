edf6df3d0999a6ff9daff95f72e823d3429b91b7
Michael Kress <m-kress@m-kress.de>
2017-11-10 13:26:25 +0100

    updated openssl to version 1.0.2m

--- oss_packages/scripts/openssl-1.0.2m.sh
+++ oss_packages/scripts/openssl-1.0.2m.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # name of directory after extracting the archive in working directory
-PKG_DIR="openssl-1.0.2l"
+PKG_DIR="openssl-1.0.2m"
 
 # name of the archive in dl directory
 PKG_ARCHIVE_FILE="${PKG_DIR}.tar.gz"
@@ -11,7 +11,7 @@ PKG_ARCHIVE_FILE="${PKG_DIR}.tar.gz"
 PKG_DOWNLOAD="https://m3-container.net/M3_Container/oss_packages/${PKG_ARCHIVE_FILE}"
 
 # md5 checksum of archive in dl directory
-PKG_CHECKSUM="f85123cd390e864dfbe517e7616e6566"
+PKG_CHECKSUM="10e9e37f492094b9ef296f68f24a7666"
 
 
 
