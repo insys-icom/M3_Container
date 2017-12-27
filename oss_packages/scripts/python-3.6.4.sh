7f402fb410da0e0146f3adfcdbb0b26280235b75
Michael Kress <m-kress@m-kress.de>
2017-12-27 13:41:37 +0100

    updated Python to version 3.6.4

--- oss_packages/scripts/python-3.6.4.sh
+++ oss_packages/scripts/python-3.6.4.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 # name of directory after extracting the archive in working directory
-PKG_DIR="Python-3.6.3"
+PKG_DIR="Python-3.6.4"
 
 # name of the archive in dl directory
 PKG_ARCHIVE_FILE="${PKG_DIR}.tar.xz"
 
 # download link for the sources to be stored in dl directory
-# https://www.python.org/ftp/python/3.6.3/Python-3.6.3.tar.xz
+# https://www.python.org/ftp/python/3.6.4/Python-3.6.4.tar.xz
 PKG_DOWNLOAD="https://m3-container.net/M3_Container/oss_packages/${PKG_ARCHIVE_FILE}"
 
 # md5 checksum of archive in dl directory
-PKG_CHECKSUM="b9c2c36c33fb89bda1fefd37ad5af9be"
+PKG_CHECKSUM="1325134dd525b4a2c3272a1a0214dd54"
 
 # Variable for the install path used in script
 PYTHON_VERSION="python3.6"
