6a0a99c710d6328fdcfcc6d3919721ac835df388
Michael Kress <m-kress@m-kress.de>
2018-01-04 15:43:11 +0100

    updated lighttpd to version 1.4.48

--- oss_packages/scripts/lighttpd-1.4.48.sh
+++ oss_packages/scripts/lighttpd-1.4.48.sh
@@ -1,25 +1,25 @@
 #!/bin/sh
 
 # name of directory after extracting the archive in working directory
-PKG_DIR="lighttpd-1.4.45"
+PKG_DIR="lighttpd-1.4.48"
 
 # name of the archive in dl directory
 PKG_ARCHIVE_FILE="${PKG_DIR}.tar.xz"
 
 # download link for the sources to be stored in dl directory
-
+# http://download.lighttpd.net/lighttpd/releases-1.4.x/lighttpd-1.4.48.tar.xz
 PKG_DOWNLOAD="https://m3-container.net/M3_Container/oss_packages/${PKG_ARCHIVE_FILE}"
-# http://download.lighttpd.net/lighttpd/releases-1.4.x/lighttpd-1.4.45.tar.xz
+
 # md5 checksum of archive in dl directory
-PKG_CHECKSUM="a128e1eda76899ce3fd115efae5fe631"
+PKG_CHECKSUM="1e3a9eb5078f481e3a8a1d0aaac8c3c8"
 
 
 
 SCRIPTSDIR=$(dirname $0)
 HELPERSDIR="${SCRIPTSDIR}/helpers"
 TOPDIR=$(realpath ${SCRIPTSDIR}/../..)
-. ${TOPDIR}/scripts/common_settings.sh
-. ${HELPERSDIR}/functions.sh
+. "${TOPDIR}/scripts/common_settings.sh"
+. "${HELPERSDIR}/functions.sh"
 PKG_ARCHIVE="${DOWNLOADS_DIR}/${PKG_ARCHIVE_FILE}"
 PKG_SRC_DIR="${SOURCES_DIR}/${PKG_DIR}"
 PKG_BUILD_DIR="${BUILD_DIR}/${PKG_DIR}"
@@ -28,10 +28,22 @@ PKG_INSTALL_DIR="${PKG_BUILD_DIR}/install"
 configure()
 {
     cd "${PKG_BUILD_DIR}"
-    export CFLAGS="${M3_CFLAGS} -I${STAGING_INCLUDE}"
-    export LDFLAGS="${M3_LDFLAGS} -L${STAGING_LIB} -lpcre"
-    export PCRECONFIG="${STAGING_DIR}/bin/pcre-config"
-    ./configure --target=${M3_TARGET} --host=${M3_TARGET} --with-openssl --with-zlib --without-bzip2 --with-webdav-props --with-webdav-locks --without-lua --with-pcre --disable-lfs --with-gdbm --without-webdav-locks
+    ./configure \
+        CFLAGS="${M3_CFLAGS} -I${STAGING_INCLUDE}" \
+        LDFLAGS="${M3_LDFLAGS} -L${STAGING_LIB} -lpcre" \
+        PCRECONFIG="${STAGING_DIR}/bin/pcre-config" \
+        --target=${M3_TARGET} \
+        --host=${M3_TARGET} \
+        --with-openssl \
+        --with-zlib \
+        --without-bzip2 \
+        --with-webdav-props \
+        --with-webdav-locks \
+        --without-lua \
+        --with-pcre \
+        --disable-lfs \
+        --with-gdbm \
+        --without-webdav-locks || exit_failure "failed to configure ${PKG_DIR}"
 }
 
 compile()
@@ -39,7 +51,7 @@ compile()
     copy_overlay
     cd "${PKG_BUILD_DIR}"
     make ${M3_MAKEFLAGS} || exit_failure "failed to build ${PKG_DIR}"
-    make DESTDIR="${PKG_INSTALL_DIR}" install
+    make DESTDIR="${PKG_INSTALL_DIR}" install || exit_failure "failed to install ${PKG_DIR} to ${PKG_INSTALL_DIR}"
 }
 
 install_staging()
