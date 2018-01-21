0a6120bd26f18c6e82fc293aa5b532bc9385ace1
Michael Kress <m-kress@m-kress.de>
2018-01-21 17:54:25 +0100

    added build script for the net analyzer darkstat

---
+++ oss_packages/scripts/darkstat_3.0.719.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+# name of directory after extracting the archive in working directory
+PKG_DIR="darkstat-3.0.719"
+
+# name of the archive in dl directory (use "none" if empty)
+PKG_ARCHIVE_FILE="${PKG_DIR}.tar.bz2"
+
+# download link for the sources to be stored in dl directory (use "none" if empty)
+# PKG_DOWNLOAD=""
+PKG_DOWNLOAD="https://m3-container.net/M3_Container/oss_packages/${PKG_ARCHIVE_FILE}"
+
+# md5 checksum of archive in dl directory (use "none" if empty)
+PKG_CHECKSUM="963145de05cb21f4d93a9c244beeaea0"
+
+
+
+SCRIPTSDIR=$(dirname $0)
+HELPERSDIR="${SCRIPTSDIR}/helpers"
+TOPDIR=$(realpath ${SCRIPTSDIR}/../..)
+. "${TOPDIR}/scripts/common_settings.sh"
+. "${HELPERSDIR}/functions.sh"
+PKG_ARCHIVE="${DOWNLOADS_DIR}/${PKG_ARCHIVE_FILE}"
+PKG_SRC_DIR="${SOURCES_DIR}/${PKG_DIR}"
+PKG_BUILD_DIR="${BUILD_DIR}/${PKG_DIR}"
+PKG_INSTALL_DIR="${PKG_BUILD_DIR}/install"
+
+configure()
+{
+    cd "${PKG_BUILD_DIR}"
+
+    ./configure CROSS_COMPILE="${M3_CROSS_COMPILE}" \
+                CFLAGS="${M3_CFLAGS} -L${STAGING_LIB} -I${STAGING_INCLUDE}" \
+                LDFLAGS="${M3_LDFLAGS} -L${STAGING_LIB}" \
+                --target="${M3_TARGET}" \
+                --host="${M3_TARGET}" \
+                --prefix="" || exit_failure "failed to configure ${PKG_DIR}"
+}
+
+compile()
+{
+    copy_overlay
+    cd "${PKG_BUILD_DIR}"
+    make HOSTCC="gcc" HOSTCFLAGS="" ${M3_MAKEFLAGS} || exit_failure "failed to build ${PKG_DIR}"
+    make DESTDIR="${PKG_INSTALL_DIR}" install || exit_failure "failed to install ${PKG_DIR} to ${PKG_INSTALL_DIR}"
+}
+
+install_staging()
+{
+    cd "${PKG_BUILD_DIR}"
+    make DESTDIR="${STAGING_DIR}" install || exit_failure "failed to install ${PKG_DIR} to ${STAGING_DIR}"
+}
+
+. ${HELPERSDIR}/call_functions.sh
