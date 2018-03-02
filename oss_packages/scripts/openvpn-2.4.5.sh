03cc8d99c4048718e7cf44b236a959012777458c
Michael Kress <m-kress@m-kress.de>
2018-03-02 11:10:01 +0100

    updated OpenVPN to version 2.4.5

--- oss_packages/scripts/openvpn-2.4.5.sh
+++ oss_packages/scripts/openvpn-2.4.5.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # name of directory after extracting the archive in working directory
-PKG_DIR="openvpn-2.4.4"
+PKG_DIR="openvpn-2.4.5"
 
 # name of the archive in dl directory
 PKG_ARCHIVE_FILE="${PKG_DIR}.tar.xz"
@@ -11,7 +11,7 @@ PKG_ARCHIVE_FILE="${PKG_DIR}.tar.xz"
 PKG_DOWNLOAD="https://m3-container.net/M3_Container/oss_packages/${PKG_ARCHIVE_FILE}"
 
 # md5 checksum of archive in dl directory
-PKG_CHECKSUM="7a2002aad1671b24457bc9432a0c5c52"
+PKG_CHECKSUM="c510ad3c8fce738c678dbcc54367c945"
 
 
 
@@ -28,18 +28,26 @@ PKG_INSTALL_DIR="${PKG_BUILD_DIR}/install"
 configure()
 {
     cd "${PKG_BUILD_DIR}"
-    export CFLAGS="${M3_CFLAGS}  -L${STAGING_LIB} -I${STAGING_INCLUDE}"
-    export LDFLAGS="${M3_LDFLAGS}  -L${STAGING_LIB}"
-    export IPROUTE=/sbin/iproute
-    ./configure --target=${M3_TARGET} --host=${M3_TARGET} --prefix="" --disable-plugin-auth-pam --disable-plugins --disable-debug --enable-iproute2 --enable-small
+    ./configure \
+        CFLAGS="${M3_CFLAGS}  -L${STAGING_LIB} -I${STAGING_INCLUDE}" \
+        LDFLAGS="${M3_LDFLAGS} -L${STAGING_LIB}" \
+        IPROUTE=/sbin/iproute \
+        --target=${M3_TARGET} \
+        --host=${M3_TARGET} \
+        --prefix="" \
+        --disable-plugin-auth-pam \
+        --disable-plugins \
+        --disable-debug \
+        --enable-iproute2 \
+        --enable-small || exit_failure "failed to configure ${PKG_DIR}"
 }
 
 compile()
 {
     copy_overlay
     cd "${PKG_BUILD_DIR}"
-    make ${M3_MAKEFLAGS} || exit_failure "failed to build ${PKG_DIR}"
-    make DESTDIR="${PKG_INSTALL_DIR}" install
+    make ${M3_MAKEFLAGS} || exit_failure "failed to build ${PKG_DIR}" || exit_failure "failed to build ${PKG_DIR}"
+    make DESTDIR="${PKG_INSTALL_DIR}" install || exit_failure "failed to install ${PKG_DIR} to ${PKG_INSTALL_DIR}"
 }
 
 install_staging()
