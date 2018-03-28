7a2ca73178ca42275f4a9f0c570f169cbcd56346
Michael Kress <m-kress@m-kress.de>
2018-03-28 15:33:35 +0200

    added selftest script for python3 container

---
+++ rootfs_skeleton/bin/selftest_python3.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+. /etc/profile
+
+# print a small python script using SSL
+echo -en "
+#!/bin/python
+try:
+    import ssl
+except ImportError:
+    pass
+else:
+    if ssl.RAND_status() is 1:
+        exit(0)
+    else:
+        exit(1)
+" > /tmp/selftest_python.py
+
+/bin/python /tmp/selftest_python.py
+exit $?
