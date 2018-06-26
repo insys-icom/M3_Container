795736e10a20cf4f98345d9ab4d223a0e7822bce
Michael Kress <m-kress@m-kress.de>
2018-06-26 07:13:25 +0200

    updated nettle to version 3.4.0

--- oss_packages/scripts/nettle-3.4.sh
+++ oss_packages/scripts/nettle-3.4.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # name of directory after extracting the archive in working directory
-PKG_DIR="nettle-3.3"
+PKG_DIR="nettle-3.4"
 
 # name of the archive in dl directory (use "none" if empty)
 PKG_ARCHIVE_FILE="${PKG_DIR}.tar.gz"
@@ -11,7 +11,7 @@ PKG_ARCHIVE_FILE="${PKG_DIR}.tar.gz"
 PKG_DOWNLOAD="https://m3-container.net/M3_Container/oss_packages/${PKG_ARCHIVE_FILE}"
 
 # md5 checksum of archive in dl directory (use "none" if empty)
-PKG_CHECKSUM="10f969f78a463704ae73529978148dbe"
+PKG_CHECKSUM="dc0f13028264992f58e67b4e8915f53d"
 
 
 
@@ -27,21 +27,21 @@ PKG_INSTALL_DIR="${PKG_BUILD_DIR}/install"
 
 configure()
 {
+    ########################
+    # compile gmp first!
+    ########################
     cd "${PKG_BUILD_DIR}"
-    ./configure CFLAGS="${M3_CFLAGS}" \
-                LDFLAGS="${M3_LDFLAGS}" \
-                --target=${M3_TARGET} \
-                --host=${M3_TARGET} \
+    ./configure CFLAGS="${M3_CFLAGS} -I${STAGING_INCLUDE}" \
+                LDFLAGS="${M3_LDFLAGS} -L${STAGING_LIB} -lgmp" \
+                --target="${M3_TARGET}" \
+                --host="${M3_TARGET}" \
                 --prefix="" \
-                --disable-shared \
+                --enable-shared \
+                --disable-static \
                 --enable-arm-neon \
-                --enable-mini-gmp \
-                --with-lib-path=${STAGING_LIB} \
-                --with-include-path=${STAGING_INCLUDE}
-
-    if [ $? -ne 0 ]; then
-        exit_failure "failed to configure ${PKG_DIR}"
-    fi
+                --with-lib-path="${STAGING_LIB}" \
+                --with-include-path="${STAGING_INCLUDE}" || exit_failure "failed to configure ${PKG_DIR}"
+#                 --enable-mini-gmp
 }
 
 compile()
@@ -49,7 +49,6 @@ compile()
     copy_overlay
     cd "${PKG_BUILD_DIR}"
     make ${M3_MAKEFLAGS} PREFIX=/ CC="${M3_CROSS_COMPILE}gcc" CFLAGS="${M3_CFLAGS} -I${STAGING_INCLUDE}" LDFLAGS="${M3_LDFLAGS} -L${STAGING_LIB}" || exit_failure "failed to build ${PKG_DIR}"
-
     make PREFIX=/ DESTDIR="${PKG_INSTALL_DIR}" install || exit_failure "failed to install ${PKG_DIR} to ${PKG_INSTALL_DIR}"
 }
 
