25262500f8a67e024bab659ab7bdb4ef86e3f11d
Michael Kress <m-kress@m-kress.de>
2018-06-26 09:59:07 +0200

    added tslib build script

---
+++ oss_packages/scripts/tslib-1.16.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+# name of directory after extracting the archive in working directory
+PKG_DIR="tslib-1.16"
+
+# name of the archive in dl directory (use "none" if empty)
+PKG_ARCHIVE_FILE="${PKG_DIR}.tar.gz"
+
+# download link for the sources to be stored in dl directory (use "none" if empty)
+# PKG_DOWNLOAD="https://github.com/kergoth/tslib/releases/download/1.16/${PKG_ARCHIVE_FILE}"
+PKG_DOWNLOAD="https://m3-container.net/M3_Container/oss_packages/${PKG_ARCHIVE_FILE}"
+
+# md5 checksum of archive in dl directory (use "none" if empty)
+PKG_CHECKSUM="0df0dc3b0dae948b92c37559b767d6f1"
+
+
+
+SCRIPTSDIR=$(dirname $0)
+HELPERSDIR="${SCRIPTSDIR}/helpers"
+TOPDIR=$(realpath ${SCRIPTSDIR}/../..)
+. "${TOPDIR}/scripts/common_settings.sh"
+. "${HELPERSDIR}/functions.sh"
+PKG_ARCHIVE="${DOWNLOADS_DIR}/${PKG_ARCHIVE_FILE}"
+PKG_SRC_DIR="${SOURCES_DIR}/${PKG_DIR}"
+PKG_BUILD_DIR="${BUILD_DIR}/${PKG_DIR}"
+PKG_INSTALL_DIR="${PKG_BUILD_DIR}/install"
+
+configure()
+{
+    cd "${PKG_BUILD_DIR}"
+    chmod +x configure
+    CFLAGS="${M3_CFLAGS} -I${STAGING_INCLUDE}" LDFLAGS="${M3_LDFLAGS} -L${STAGING_LIB}" ./configure --host=${M3_TARGET} \
+        --prefix="" \
+        ac_cv_func_malloc_0_nonnull=yes \
+        || exit_failure "failed to configure ${PKG_DIR}"
+}
+
+compile()
+{
+    copy_overlay
+    cd "${PKG_BUILD_DIR}"
+    make ${M3_MAKEFLAGS} || exit_failure "failed to build ${PKG_DIR}"
+    make DESTDIR="${PKG_INSTALL_DIR}" install
+}
+
+install_staging()
+{
+    cd "${PKG_BUILD_DIR}"
+    make DESTDIR="${STAGING_DIR}" install || exit_failure "failed to install ${PKG_DIR}"
+}
+
+. ${HELPERSDIR}/call_functions.sh
