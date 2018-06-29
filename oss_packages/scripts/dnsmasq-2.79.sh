b601553a942fc6997a9160c7610839f676927121
Michael Kress <m-kress@m-kress.de>
2018-06-29 09:19:26 +0200

    updated dnsmasq build script

--- oss_packages/scripts/dnsmasq-2.79.sh
+++ oss_packages/scripts/dnsmasq-2.79.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # name of directory after extracting the archive in working directory
-PKG_DIR="dnsmasq-2.78"
+PKG_DIR="dnsmasq-2.79"
 
 # name of the archive in dl directory (use "none" if empty)
 PKG_ARCHIVE_FILE="${PKG_DIR}.tar.xz"
@@ -11,7 +11,7 @@ PKG_ARCHIVE_FILE="${PKG_DIR}.tar.xz"
 PKG_DOWNLOAD="https://m3-container.net/M3_Container/oss_packages/${PKG_ARCHIVE_FILE}"
 
 # md5 checksum of archive in dl directory (use "none" if empty)
-PKG_CHECKSUM="6d0241b72c79d2b510776ccc4ed69ca4"
+PKG_CHECKSUM="64010c9734f5e275eb591374a6dad924"
 
 
 
