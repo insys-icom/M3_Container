3c0317f37730745948c4dc644f2067bdbbd1f0d4
Michael Kress <m-kress@m-kress.de>
2018-07-22 13:27:43 +0200

    updated ncurses to version 6.1

---
+++ oss_packages/scripts/ncurses-6.1.sh
@@ -0,0 +1,64 @@
+#!/bin/sh
+
+# name of directory after extracting the archive in working directory
+PKG_DIR="ncurses-6.1"
+
+# name of the archive in dl directory
+PKG_ARCHIVE_FILE="${PKG_DIR}.tar.gz"
+
+# download link for the sources to be stored in dl directory
+# PKG_DOWNLOAD="https://invisible-mirror.net/archives/ncurses/${PKG_ARCHIVE_FILE}"
+PKG_DOWNLOAD="https://m3-container.net/M3_Container/oss_packages/${PKG_ARCHIVE_FILE}"
+
+# md5 checksum of archive in dl directory
+PKG_CHECKSUM="98c889aaf8d23910d2b92d65be2e737a"
+
+
+
+SCRIPTSDIR=$(dirname $0)
+HELPERSDIR="${SCRIPTSDIR}/helpers"
+TOPDIR=$(realpath ${SCRIPTSDIR}/../..)
+. ${TOPDIR}/scripts/common_settings.sh
+. ${HELPERSDIR}/functions.sh
+PKG_ARCHIVE="${DOWNLOADS_DIR}/${PKG_ARCHIVE_FILE}"
+PKG_SRC_DIR="${SOURCES_DIR}/${PKG_DIR}"
+PKG_BUILD_DIR="${BUILD_DIR}/${PKG_DIR}"
+PKG_INSTALL_DIR="${PKG_BUILD_DIR}/install"
+
+configure()
+{
+    cd "${PKG_BUILD_DIR}"
+    CFLAGS="${M3_CFLAGS}" \
+        ./configure \
+        --target=${M3_TARGET} \
+        --host=${M3_TARGET} \
+        --prefix="/" \
+        --with-termlib \
+        --enable-static \
+        --with-shared \
+        --without-cxx \
+        --without-ada \
+        --without-manpages \
+        --without-progs \
+        --without-tests \
+        --disable-big-core \
+        --disable-home-terminfo \
+        --without-develop \
+        --enable-widec || exit_failure "failed to configure ${PKG_DIR}"
+}
+
+compile()
+{
+    copy_overlay
+    cd "${PKG_BUILD_DIR}"
+    make ${M3_MAKEFLAGS} || exit_failure "failed to build ${PKG_DIR}"
+    make DESTDIR="${PKG_INSTALL_DIR}" install || exit_failure "failed to install ${PKG_DIR} to ${PKG_INSTALL_DIR}"
+}
+
+install_staging()
+{
+    cd "${PKG_BUILD_DIR}"
+    make DESTDIR="${STAGING_DIR}" install || exit_failure "failed to install ${PKG_DIR}"
+}
+
+. ${HELPERSDIR}/call_functions.sh
