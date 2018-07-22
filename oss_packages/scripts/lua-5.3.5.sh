515e7da7e49bde2cbd4facfbb1782972b4defa1f
Michael Kress <m-kress@m-kress.de>
2018-07-22 15:49:40 +0200

    updated lua to version 5.3.5

--- oss_packages/scripts/lua-5.3.5.sh
+++ oss_packages/scripts/lua-5.3.5.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
 # name of directory after extracting the archive in working directory
-PKG_DIR="lua-5.3.3"
+PKG_DIR="lua-5.3.5"
 
 # name of the archive in dl directory
 PKG_ARCHIVE_FILE="${PKG_DIR}.tar.gz"
 
 # download link for the sources to be stored in dl directory
+# PKG_DOWNLOAD="https://www.lua.org/ftp/lua-5.3.5.tar.gz"
 PKG_DOWNLOAD="https://m3-container.net/M3_Container/oss_packages/${PKG_ARCHIVE_FILE}"
 
 # md5 checksum of archive in dl directory
-PKG_CHECKSUM="703f75caa4fdf4a911c1a72e67a27498"
+PKG_CHECKSUM="4f4b4f323fd3514a68e0ab3da8ce3455"
 
 
 
@@ -33,7 +34,16 @@ compile()
 {
     copy_overlay
     cd "${PKG_BUILD_DIR}"
-    make ${M3_MAKEFLAGS} CC="${M3_CROSS_COMPILE}gcc" CFLAGS="-Wall -Wextra -DLUA_COMPAT_5_2 -fPIC -ULUA_USE_READLINE ${M3_CFLAGS}" LUA_LIBS="" SYSCFLAGS="-DLUA_USE_LINUX" SYSLDFLAGS="${M3_LDFLAGS} -L${STAGING_LIB}" AR="${AR} rcu" RANLIB="${RANLIB}" NM="${NM}" generic || exit_failure "failed to build ${PKG_DIR}"
+    make ${M3_MAKEFLAGS} \
+        CC="${M3_CROSS_COMPILE}gcc" \
+        CFLAGS="-Wall -Wextra -DLUA_COMPAT_5_2 -fPIC -ULUA_USE_READLINE ${M3_CFLAGS}" \
+        LUA_LIBS="" \
+        SYSCFLAGS="-DLUA_USE_LINUX" \
+        SYSLDFLAGS="${M3_LDFLAGS} -L${STAGING_LIB}" \
+        AR="${AR} rcu" \
+        RANLIB="${RANLIB}" \
+        NM="${NM}" \
+        generic || exit_failure "failed to build ${PKG_DIR}"
     make local
 }
 
