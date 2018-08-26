5ee59b82883f5966f9d6eb731909ab1750e27095
Michael Kress <m-kress@m-kress.de>
2018-08-26 19:00:30 +0200

    added build script for minetest 0.4.17.1

---
+++ oss_packages/scripts/minetest_game-0.4.17.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+# name of directory after extracting the archive in working directory
+PKG_DIR="minetest_game-0.4.17"
+
+# name of the archive in dl directory (use "none" if empty)
+PKG_ARCHIVE_FILE="${PKG_DIR}.tar.gz"
+
+# download link for the sources to be stored in dl directory (use "none" if empty)
+# PKG_DOWNLOAD="https://github.com/minetest/minetest_game/archive/0.4.17.1.tar.gz"
+PKG_DOWNLOAD="https://m3-container.net/M3_Container/oss_packages/${PKG_ARCHIVE_FILE}"
+
+# md5 checksum of archive in dl directory (use "none" if empty)
+PKG_CHECKSUM="44b2dc15697430992ba745bdee917dc7"
+
+
+
+SCRIPTSDIR=$(dirname $0)
+HELPERSDIR="${SCRIPTSDIR}/helpers"
+TOPDIR=$(realpath ${SCRIPTSDIR}/../..)
+. "${TOPDIR}/scripts/common_settings.sh"
+. "${HELPERSDIR}/functions.sh"
+PKG_ARCHIVE="${DOWNLOADS_DIR}/${PKG_ARCHIVE_FILE}"
+PKG_SRC_DIR="${SOURCES_DIR}/${PKG_DIR}"
+PKG_BUILD_DIR="${BUILD_DIR}/${PKG_DIR}"
+PKG_INSTALL_DIR="${PKG_BUILD_DIR}/install"
+
+configure()
+{
+    true
+}
+
+compile()
+{
+    true
+}
+
+install_staging()
+{
+    mkdir -p "${STAGING_DIR}/usr/local/games"
+    cp -r "${PKG_BUILD_DIR}" "${STAGING_DIR}/usr/local/games/minetest_game"
+}
+
+uninstall_staging()
+{
+    rm -rf "${STAGING_DIR}/usr/local/games/minetest_game"
+}
+
+. ${HELPERSDIR}/call_functions.sh
