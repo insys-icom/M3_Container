dc8b9386bc7156c9f5ead566e5a35d87655b2d63
Michael Kress <m-kress@m-kress.de>
2019-01-10 13:55:00 +0100

    updated bird to version 1.6.5

--- oss_packages/scripts/bird-1.6.5.sh
+++ oss_packages/scripts/bird-1.6.5.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # name of directory after extracting the archive in working directory
-PKG_DIR="bird-1.6.4"
+PKG_DIR="bird-1.6.5"
 
 # name of the archive in dl directory (use "none" if empty)
 PKG_ARCHIVE_FILE="${PKG_DIR}.tar.gz"
@@ -11,7 +11,7 @@ PKG_ARCHIVE_FILE="${PKG_DIR}.tar.gz"
 PKG_DOWNLOAD="https://m3-container.net/M3_Container/oss_packages/${PKG_ARCHIVE_FILE}"
 
 # md5 checksum of archive in dl directory (use "none" if empty)
-PKG_CHECKSUM="d62ec2547338e8d3dfb934b4c7b2faa4"
+PKG_CHECKSUM="63c0e11d553bf07163e802ab9217a8d0"
 
 
 
