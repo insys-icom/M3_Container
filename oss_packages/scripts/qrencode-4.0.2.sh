b1bd5bc85bcb642a63bbf4e6f3b4a228b3fcc536
Michael Kress <m-kress@m-kress.de>
2019-01-18 09:04:52 +0100

    added build script for qrencode

---
+++ oss_packages/scripts/qrencode-4.0.2.sh
@@ -0,0 +1,60 @@
+#! /bin/sh
+
+# name of directory after extracting the archive in working directory
+PKG_DIR="qrencode-4.0.2"
+
+# name of the archive in dl directory (use "none" if empty)
+PKG_ARCHIVE_FILE="${PKG_DIR}.tar.gz"
+
+# download link for the sources to be stored in dl directory (use "none" if empty)
+# https://sourceforge.net/projects/libpng/files/libpng16/1.6.36/libpng-1.6.36.tar.xz/download
+PKG_DOWNLOAD="https://m3-container.net/M3_Container/oss_packages/${PKG_ARCHIVE_FILE}"
+
+# md5 checksum of archive in dl directory (use "none" if empty)
+PKG_CHECKSUM="4946993cb59c7adf63e5ff5370635853"
+
+
+
+SCRIPTSDIR=$(dirname $0)
+HELPERSDIR="${SCRIPTSDIR}/helpers"
+TOPDIR=$(realpath ${SCRIPTSDIR}/../..)
+. ${TOPDIR}/scripts/common_settings.sh
+. ${HELPERSDIR}/functions.sh
+PKG_ARCHIVE="${DOWNLOADS_DIR}/${PKG_ARCHIVE_FILE}"
+PKG_SRC_DIR="${SOURCES_DIR}/${PKG_DIR}"
+PKG_BUILD_DIR="${BUILD_DIR}/${PKG_DIR}"
+PKG_INSTALL_DIR="${PKG_BUILD_DIR}/install"
+
+configure()
+{
+    cd "${PKG_BUILD_DIR}"
+    ./configure \
+        CFLAGS="${M3_CFLAGS} -I${STAGING_INCLUDE}" \
+        CPPFLAGS="-I${STAGING_INCLUDE}" \
+        LDFLAGS="${M3_LDFLAGS} -L${STAGING_LIB}" \
+        png_CFLAGS="-I${STAGING_INCLUDE}" \
+        png_LIBS="${STAGING_LIB}/libpng.a -lz" \
+        --target=${M3_TARGET} \
+        --host=${M3_TARGET} \
+        --program-prefix="" \
+        --disable-shared \
+        --enable-static \
+        --prefix="" \
+        || exit_failure "failed to configure ${PKG_DIR}"
+}
+
+compile()
+{
+    copy_overlay
+    cd "${PKG_BUILD_DIR}"
+    make ${M3_MAKEFLAGS} || exit_failure "failed to build ${PKG_DIR}"
+    make DESTDIR="${PKG_INSTALL_DIR}" install || exit_failure "failed to install ${PKG_DIR} to ${PKG_INSTALL_DIR}"
+}
+
+install_staging()
+{
+    cd "${PKG_BUILD_DIR}"
+    make DESTDIR="${STAGING_DIR}" install || exit_failure "failed to install ${PKG_DIR} to ${STAGING_DIR}"
+}
+
+. ${HELPERSDIR}/call_functions.sh
