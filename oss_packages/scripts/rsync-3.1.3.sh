f7812513baa7551ff6a974557394f180e5dbd168
Michael Kress <m-kress@m-kress.de>
2019-02-13 14:57:46 +0100

    updated rsync to version 3.1.3

--- oss_packages/scripts/rsync-3.1.3.sh
+++ oss_packages/scripts/rsync-3.1.3.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # name of directory after extracting the archive in working directory
-PKG_DIR="rsync-3.1.2"
+PKG_DIR="rsync-3.1.3"
 
 # name of the archive in dl directory
 PKG_ARCHIVE_FILE="${PKG_DIR}.tar.gz"
@@ -10,7 +10,7 @@ PKG_ARCHIVE_FILE="${PKG_DIR}.tar.gz"
 PKG_DOWNLOAD="https://m3-container.net/M3_Container/oss_packages/${PKG_ARCHIVE_FILE}"
 
 # md5 checksum of archive in dl directory
-PKG_CHECKSUM="0f758d7e000c0f7f7d3792610fad70cb"
+PKG_CHECKSUM="1581a588fde9d89f6bc6201e8129afaf"
 
 
 
@@ -27,10 +27,12 @@ PKG_INSTALL_DIR="${PKG_BUILD_DIR}/install"
 configure()
 {
     cd "${PKG_BUILD_DIR}"
-    #export CROSS_COMPILE="${M3_CROSS_COMPILE}"
-    export CFLAGS="${M3_CFLAGS} -L${STAGING_LIB} -I${STAGING_INCLUDE}"
-    export LDFLAGS="${M3_LDFLAGS} -L${STAGING_LIB}"
-    ./configure --target=${M3_TARGET} --host=${M3_TARGET} --prefix=""
+    ./configure \
+        CFLAGS="${M3_CFLAGS} -L${STAGING_LIB} -I${STAGING_INCLUDE}" \
+        LDFLAGS="${M3_LDFLAGS} -L${STAGING_LIB}" \
+        --target=${M3_TARGET} \
+        --host=${M3_TARGET} \
+        --prefix="" || exit_failure "failed to configure ${PKG_DIR}"
 }
 
 compile()
@@ -38,7 +40,7 @@ compile()
     copy_overlay
     cd "${PKG_BUILD_DIR}"
     make ${M3_MAKEFLAGS} || exit_failure "failed to build ${PKG_DIR}"
-    make DESTDIR="${PKG_INSTALL_DIR}" install
+    make DESTDIR="${PKG_INSTALL_DIR}" install || exit_failure "failed to install ${PKG_DIR} to ${STAGING_DIR}"
 }
 
 install_staging()
