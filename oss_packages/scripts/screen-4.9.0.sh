a8ad172723e42537d6636b31819864213c767384
Michael Kress <m-kress@m-kress.de>
2022-05-05 22:23:29 +0200

    add build script for screen

---
+++ oss_packages/scripts/screen-4.9.0.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+
+# name of directory after extracting the archive in working directory
+PKG_DIR="screen-4.9.0"
+
+# name of the archive in dl directory (use "none" if empty)
+PKG_ARCHIVE_FILE="${PKG_DIR}.tar.gz"
+
+# download link for the sources to be stored in dl directory (use "none" if empty)
+PKG_DOWNLOAD="https://ftp.gnu.org/gnu/screen/${PKG_ARCHIVE_FILE}"
+
+# md5 checksum of archive in dl directory (use "none" if empty)
+PKG_CHECKSUM="b1ef8ed89134d335e614016634982b6d"
+
+
+
+SCRIPTSDIR=$(dirname $0)
+HELPERSDIR="${SCRIPTSDIR}/helpers"
+TOPDIR=$(realpath ${SCRIPTSDIR}/../..)
+. "${TOPDIR}/scripts/common_settings.sh"
+. "${HELPERSDIR}/functions.sh"
+PKG_ARCHIVE="${DOWNLOADS_DIR}/${PKG_ARCHIVE_FILE}"
+PKG_SRC_DIR="${SOURCES_DIR}/${PKG_DIR}"
+PKG_BUILD_DIR="${BUILD_DIR}/${PKG_DIR}"
+PKG_INSTALL_DIR="${PKG_BUILD_DIR}/install"
+
+configure()
+{
+    cd "${PKG_BUILD_DIR}"
+    ./autogen.sh
+
+    ./configure CROSS_COMPILE="${M3_CROSS_COMPILE}" \
+        CFLAGS="${M3_CFLAGS} -L${STAGING_LIB} -I${STAGING_INCLUDE}" \
+        LDFLAGS="${M3_LDFLAGS} -L${STAGING_LIB}" \
+        --target="${M3_TARGET}" \
+        --host="${M3_TARGET}" \
+        --prefix="" \
+        --enable-pam \
+        --enable-use-locale=no \
+        --enable-colors256 \
+        --enable-rxvt_osc \
+        || exit_failure "failed to configure ${PKG_DIR}"
+}
+
+compile()
+{
+    copy_overlay
+    cd "${PKG_BUILD_DIR}"
+    make ${M3_MAKEFLAGS} || exit_failure "failed to build ${PKG_DIR}"
+    make DESTDIR="${PKG_INSTALL_DIR}" install || exit_failure "failed to install ${PKG_DIR} to ${PKG_INSTALL_DIR}"
+}
+
+install_staging()
+{
+    cd "${PKG_BUILD_DIR}"
+    make DESTDIR="${STAGING_DIR}" install || exit_failure "failed to install ${PKG_DIR} to ${STAGING_DIR}"
+}
+
+. ${HELPERSDIR}/call_functions.sh
