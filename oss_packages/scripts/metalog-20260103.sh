c5b5516beece21032aad29912350185f3d3f5887
Michael Kress <m-kress@m-kress.de>
2026-01-03 13:41:13 +0100

    add updated metalog build script

---
+++ oss_packages/scripts/metalog-20260103.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+
+# name of directory after extracting the archive in working directory
+PKG_DIR="metalog-20260103"
+
+# name of the archive in dl directory (use "none" if empty)
+PKG_ARCHIVE_FILE="${PKG_DIR}.tar.gz"
+
+# download link for the sources to be stored in dl directory (use "none" if empty)
+#PKG_DOWNLOAD="https://github.com/hvisage/metalog/archive/refs/tags/${PKG_ARCHIVE_FILE#*-}"
+PKG_DOWNLOAD="https://m3-container.net/M3_Container/oss_packages/${PKG_ARCHIVE_FILE}"
+
+# md5 checksum of archive in dl directory (use "none" if empty)
+PKG_CHECKSUM="da762375999762069d0cd546b2d8f6b143dbd5677dbb68443e4b455f825e897e"
+
+
+
+SCRIPTSDIR=$(dirname $0)
+HELPERSDIR="${SCRIPTSDIR}/helpers"
+TOPDIR=$(realpath ${SCRIPTSDIR}/../..)
+. "${TOPDIR}"/scripts/common_settings.sh
+. "${HELPERSDIR}"/functions.sh
+PKG_ARCHIVE="${DOWNLOADS_DIR}/${PKG_ARCHIVE_FILE}"
+PKG_SRC_DIR="${SOURCES_DIR}/${PKG_DIR}"
+PKG_BUILD_DIR="${BUILD_DIR}/${PKG_DIR}"
+PKG_INSTALL_DIR="${PKG_BUILD_DIR}/install"
+
+configure()
+{
+    cd "${PKG_BUILD_DIR}"
+    ./autogen.sh
+    ./configure \
+        CFLAGS="${M3_CFLAGS}" \
+        LDFLAGS="${M3_LDFLAGS}" \
+        PCRE2_CFLAGS="-I${STAGING_INCLUDE}" \
+        PCRE2_LIBS="-L${STAGING_LIB} -lpcre2-8" \
+        ZLIB_CFLAGS="-I${STAGING_INCLUDE}" \
+        ZLIB_LIBS="-L${STAGING_LIB} -lz" \
+        --target="${M3_TARGET}" \
+        --host="${M3_TARGET}" \
+        --prefix="" \
+        --with-compress \
+        --with-unicode \
+        || exit_failure "failed to configure ${PKG_DIR}"
+}
+
+compile()
+{
+    copy_overlay
+    cd "${PKG_BUILD_DIR}"
+    make "${M3_MAKEFLAGS}" || exit_failure "failed to build ${PKG_DIR}"
+    make DESTDIR="${PKG_INSTALL_DIR}" install || exit_failure "failed to install ${PKG_DIR} to ${PKG_INSTALL_DIR}"
+}
+
+install_staging()
+{
+    cd "${PKG_BUILD_DIR}"
+    make DESTDIR="${STAGING_DIR}" install || exit_failure "failed to install ${PKG_DIR}"
+}
+
+. ${HELPERSDIR}/call_functions.sh
